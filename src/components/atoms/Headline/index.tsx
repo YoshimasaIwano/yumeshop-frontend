98921ae99f8ab9d7828cb4504b55665912043594
YoshimasaIwano <y_iwano@MacBook-Pro.local>
2023-05-22 16:42:50 +0900

    feat: add headline styles

--- src/components/atoms/Headline/index.tsx
+++ src/components/atoms/Headline/index.tsx
@@ -1,18 +1,24 @@
-import { VFC } from 'react';
+import styles from './styles.module.scss';
 
 interface HeadlineProps {
   label: string;
   headlineTypes: 'small' | 'middle' | 'large';
 }
 
-export const Headline: VFC<HeadlineProps> = (props) => {
+export const Headline: React.FC<HeadlineProps> = (props) => {
   const { label, headlineTypes } = props;
 
   return (
     <>
-      {headlineTypes === 'large' && <h1>{label}</h1>}
-      {headlineTypes === 'middle' && <h2>{label}</h2>}
-      {headlineTypes === 'small' && <h3>{label}</h3>}
+      {headlineTypes === 'large' && (
+        <h1 className={styles.headline}>{label}</h1>
+      )}
+      {headlineTypes === 'middle' && (
+        <h2 className={styles.headline}>{label}</h2>
+      )}
+      {headlineTypes === 'small' && (
+        <h3 className={styles.headline}>{label}</h3>
+      )}
     </>
   );
 };
