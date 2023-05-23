5bde59d8c2fd689b04aed72da4754dd35e66385c
Yoshimasa Iwano <63036747+YoshimasaIwano@users.noreply.github.com>
2023-05-23 17:43:44 +0900

    Update src/styles/Home.ts
    Co-authored-by: Takuya Sakoda <syakoo.pub@gmail.com>

--- src/styles/Home.ts
+++ src/styles/Home.ts
@@ -12,12 +12,6 @@ export const Container = styled.div`
 export const Main = styled.main`
   background: ${colors.YumeWhiteGreen};
   min-height: 100vh;
-  // padding: 4rem 0;
-  // flex: 1;
-  // display: flex;
-  // flex-direction: column;
-  // justify-content: center;
-  // align-items: center;
 `;
 
 export const Title = styled.h1`
